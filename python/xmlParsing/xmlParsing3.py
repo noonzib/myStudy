b01af4e53d654eea9ef42d1f88aafa1ac9dcc528
noonzib <noonzib14@gmail.com>
2019-06-20 16:48:10 +0900

    update xml parser

--- python/xmlParsing/xmlParsing3.py
+++ python/xmlParsing/xmlParsing3.py
@@ -3,17 +3,53 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('./data.xml')
 root = tree.getroot()
 
-print root.tag, root.attrib
-cnt = 0
+ftext = "2011"
+ttext = "7777"
 
+cnt = 0
+print root.tag, root.attrib
 def findChild(parent, cnt):
 	cnt += 1 
+	
 	for child in parent:
 		if child != "":
-			print "\t|"*cnt+'-->',child.tag, child.attrib#, "" if not(child.text.isalpha()) else child.text	
+			print "\t|"*cnt+'-->',child.tag, child.attrib, child.text#, "" if child.text.isspace() else child.text	
 			findChild(child, cnt)
 		else:
-	 		break
+			break
+	pass
 
-findChild(root,cnt)
+def findAll():
+	for child in root.iter('*'):
+		print child.tag,child.attrib,child.text
+	pass
 
+def changeAllText():
+	global ftext, ttext
+	for child in root.iter('*'):
+		if child.text == ftext:
+			child.text = ttext
+			print child.text
+			child.set('updated', 'yes')
+			print "changed the text"
+			pass
+	tree.write('data.xml')
+	pass
+
+def changeChooseText():
+	idx = 1 
+	imsi = 0
+	global ftext, ttext
+	for child in root.iter('*'):
+		if child.text == ftext:
+			child.text = ttext
+			print child.text
+			child.set('updated', 'yes')
+			print "changed the text"
+			pass
+	tree.write('data.xml')
+	pass
+
+#changeAllText()
+findChild(root, cnt)
+findAll()
