1491c2ead9ce788bb0e93f6fef0802fcf31ee29b
noonzib <noonzib14@gmail.com>
2019-06-21 08:37:04 +0900

    Update the xml parser

--- python/xmlParsing/xmlParsing3.py
+++ python/xmlParsing/xmlParsing3.py
@@ -3,14 +3,13 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('./data.xml')
 root = tree.getroot()
 
-ftext = "2011"
-ttext = "7777"
+ftext = "7777"
+ttext = "8888"
 
 cnt = 0
 print root.tag, root.attrib
 def findChild(parent, cnt):
 	cnt += 1 
-	
 	for child in parent:
 		if child != "":
 			print "\t|"*cnt+'-->',child.tag, child.attrib, child.text#, "" if child.text.isspace() else child.text	
@@ -36,20 +35,24 @@ def changeAllText():
 	tree.write('data.xml')
 	pass
 
-def changeChooseText():
-	idx = 1 
+def changeChooseText(idx): 
 	imsi = 0
 	global ftext, ttext
 	for child in root.iter('*'):
 		if child.text == ftext:
-			child.text = ttext
-			print child.text
-			child.set('updated', 'yes')
-			print "changed the text"
-			pass
-	tree.write('data.xml')
+			imsi+=1
+			if imsi==idx:
+				child.text = ttext
+				print child.text
+				child.set('updated', 'yes')
+				tree.write('data.xml')
+				print "changed the text"
+				return
+	print "Can't find the text"
 	pass
 
-#changeAllText()
-findChild(root, cnt)
-findAll()
+# findChild(root, cnt)
+# findAll()
+idx = 2
+# changeAllText()
+changeChooseText(idx)
