f4ff93cb2b48f0f85d9f663cb31e74e739c4da88
noonzib <noonzib14@gmail.com>
2019-06-21 10:11:26 +0900

    Update xml parser solved the problem
    remove the whitespace

--- python/xmlParsing/xmlParsing3.py
+++ python/xmlParsing/xmlParsing3.py
@@ -1,14 +1,24 @@
 import xml.etree.ElementTree as ET
+from lxml import etree
 
-tree = ET.parse('./data.xml')
-root = tree.getroot()
+parser = etree.XMLParser(remove_blank_text=True)
+with open('data.xml', 'r') as file:
+	data = file.read()
+elem = etree.XML(data, parser = parser)
+root = ET.fromstring(etree.tostring(elem))
 
-ftext = "7777"
-ttext = "8888"
+# print data
+# print etree.tostring(elem)
+
+# tree = ET.parse('./data.xml')
+# root = tree.getroot()
 
 cnt = 0
-print root.tag, root.attrib
+
 def findChild(parent, cnt):
+	if cnt == 0:
+		print root.tag, root.attrib
+		pass
 	cnt += 1 
 	for child in parent:
 		if child != "":
@@ -20,24 +30,28 @@ def findChild(parent, cnt):
 
 def findAll():
 	for child in root.iter('*'):
-		print child.tag,child.attrib,child.text
+		print child.tag,child.text
 	pass
 
-def changeAllText():
-	global ftext, ttext
+def changeAllText(ftext, ttext):
+	token = 0
 	for child in root.iter('*'):
 		if child.text == ftext:
 			child.text = ttext
 			print child.text
 			child.set('updated', 'yes')
 			print "changed the text"
+			token = 1
 			pass
-	tree.write('data.xml')
+	if token != 0:
+		tree.write('data.xml')
+		pass
+	else:
+		print "Can't find the text"
 	pass
 
-def changeChooseText(idx): 
+def changeChooseText(ftext,ttext,idx): 
 	imsi = 0
-	global ftext, ttext
 	for child in root.iter('*'):
 		if child.text == ftext:
 			imsi+=1
@@ -51,8 +65,33 @@ def changeChooseText(idx):
 	print "Can't find the text"
 	pass
 
-# findChild(root, cnt)
-# findAll()
-# changeAllText()
+def printText(parent,cnt):
+	cnt += 1
+	for child in parent:
+		print "\t|"*cnt+'-->',child.text
+		printText(child,cnt)
+		pass
+	pass
+
+#findChild(root, cnt)
+#findAll()
+#printText(root,cnt)
+########################Replace text###########################
 idx = 2
-changeChooseText(idx)
+ftext = "7777"
+ttext = "8888"
+#changeAllText(ftext, ttext)
+#changeChooseText(ftext,ttext,idx)
+###############################################################
+
+print root[1][2].text[5]
+
+
+
+
+
+
+
+
+
+
---
+++ python/xmlParsing/xml_remove_whitespace.py
@@ -0,0 +1,7 @@
+from lxml import etree
+parser = etree.XMLParser(remove_blank_text=True)
+with open('data.xml', 'r') as file:
+	data = file.read()
+#print data
+elem = etree.XML(data, parser = parser)
+print etree.tostring(elem)
