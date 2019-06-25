7dee43199236ee8e4e7ea1e86088c1f023acd3d7
noonzib <noonzib14@gmail.com>
2019-06-25 12:43:52 +0900

    Update xml_remove_whitespace.py

--- python/xmlParsing/xml_remove_whitespace.py
+++ python/xmlParsing/xml_remove_whitespace.py
@@ -3,5 +3,6 @@ parser = etree.XMLParser(remove_blank_text=True)
 with open('data.xml', 'r') as file:
 	data = file.read()
 #print data
+
 elem = etree.XML(data, parser = parser)
 print etree.tostring(elem)
